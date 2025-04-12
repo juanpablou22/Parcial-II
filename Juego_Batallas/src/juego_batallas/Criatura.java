e477d2128b74ed52eecd1e5b88715abf64a1d81c
estudiante103 <estudiante103@103-08.unicamacho.co>
2025-04-11 19:16:54 -0500

    Creacion de la clase main y clase abstracta criatura

---
+++ Juego_Batallas/src/juego_batallas/Criatura.java
@@ -0,0 +1,12 @@
+
+package juego_batallas;
+
+
+public abstract class  Criatura {
+    private String nombre;
+    private int fuerza;
+    private int salud;
+    
+    
+    
+}
---
+++ Juego_Batallas/src/juego_batallas/Juego_Batallas.java
@@ -0,0 +1,12 @@
+
+package juego_batallas;
+
+
+public class Juego_Batallas {
+
+    
+    public static void main(String[] args) {
+        
+    }
+    
+}
